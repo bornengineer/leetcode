58a2eca7ea5ac993e7daf91636fe479bfeb46d30
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-08-28 01:34:45 +0530

    Time: 111 ms (44.31%), Space: 34.7 MB (30.84%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-    vector<int> findDuplicates(vector<int>& nums) {
-        unordered_map<int,int> mpp;
+//     vector<int> findDuplicates(vector<int>& nums) {
+//         unordered_map<int,int> mpp;
         
-        for(auto i:nums)mpp[i]++;
+//         for(auto i:nums)mpp[i]++;
         
-        vector<int> ans;
+//         vector<int> ans;
         
-        // for(int i = 1; i<=nums.size(); i++){
-        //     if(mpp[i] > 1)ans.push_back(i);
-        //     // cout<<i<<" "<<mpp[i]<<endl;
-        // }
+//         // for(int i = 1; i<=nums.size(); i++){
+//         //     if(mpp[i] > 1)ans.push_back(i);
+//         //     // cout<<i<<" "<<mpp[i]<<endl;
+//         // }
         
-        // or
-        for(auto i: mpp)if(i.second > 1)ans.push_back(i.first);
+//         // or
+//         for(auto i: mpp)if(i.second > 1)ans.push_back(i.first);
                 
+//         return ans;
+//     }
+    
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> freq(nums.size());
+        vector<int> ans;
+        for(int i = 0; i<nums.size(); i++){
+            freq[nums[i]-1]++;
+        }
+        
+        for(int i = 0 ; i<nums.size(); i++)if(freq[i] > 1)ans.push_back(i+1);
+        // for(auto i:freq)cout<<i<<" ";  
+        
         return ans;
     }
+
 };
