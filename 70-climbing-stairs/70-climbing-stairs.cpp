b69a4df0320db605997282286660c2378739f0b1
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-01 02:30:55 +0530

    Time: 3 ms (36.17%), Space: 6.2 MB (15.98%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -10,16 +10,28 @@ public:
     
     
     // using memoezation TC - O(N)
-    int helper(int n , vector<int>& memo){
-        if(n < 0)return 0; 
-        if(n == 0)return 1;
-        if(memo[n])return memo[n];
-        memo[n] = helper(n-1, memo) + helper(n-2, memo);
-        return memo[n];       
-    }
+//     int helper(int n , vector<int>& memo){
+//         if(n < 0)return 0; 
+//         if(n == 0)return 1;
+//         if(memo[n])return memo[n];
+//         memo[n] = helper(n-1, memo) + helper(n-2, memo);
+//         return memo[n];       
+//     }
     
+//     int climbStairs(int n) {
+//         vector<int> memo(n+1);
+//         return helper(n, memo);
+//     }
+    
+    // using bottom up dp
     int climbStairs(int n) {
-        vector<int> memo(n+1);
-        return helper(n, memo);
+        if(n <= 3)return n;
+        vector<int> dp(n+1);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i<=n ; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
