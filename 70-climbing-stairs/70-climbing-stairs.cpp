74665d8a95801e060ec8723bda109f1f4466843f
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-01 02:21:47 +0530

    Time: 4 ms (22.55%), Space: 6.3 MB (15.98%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    // Recursive approach but it will give TLE (it will accept till n = 41)
+    // int climbStairs(int n) {
+    // if(n<0)return 0;
+    // if(n==0)return 1;
+		    
+    // return climbStairs(n-1)+climbStairs(n-2);
+    // }
+    
+    
+    // using memoezation TC - O(N)
+    int helper(int n , vector<int>& memo){
+        // if(n < 0)return 0; 
+        // if(n == 0)return 1;
+        if(n == 1)return 1;
+        if(n == 2)return 2;
+        if(memo[n])return memo[n];
+        memo[n] = helper(n-1, memo) + helper(n-2, memo);
+        return memo[n];       
+    }
+    
+    int climbStairs(int n) {
+        vector<int> memo(n+1);
+        return helper(n, memo);
+    }
+};
