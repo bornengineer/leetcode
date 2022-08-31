8c1fc6c18edb4146124a8b32de800420a47be04e
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-01 02:22:26 +0530

    Time: 3 ms (36.17%), Space: 6.3 MB (15.98%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -11,10 +11,10 @@ public:
     
     // using memoezation TC - O(N)
     int helper(int n , vector<int>& memo){
-        if(n < 0)return 0; 
-        if(n == 0)return 1;
-        // if(n == 1)return 1;
-        // if(n == 2)return 2;
+        // if(n < 0)return 0; 
+        // if(n == 0)return 1;
+        if(n == 1)return 1;
+        if(n == 2)return 2;
         if(memo[n])return memo[n];
         memo[n] = helper(n-1, memo) + helper(n-2, memo);
         return memo[n];       
