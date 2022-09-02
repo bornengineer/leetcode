21891421ea3f982643b10fc77af7fbb81a63417d
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-03 02:35:37 +0530

    Time: 5 ms (28.59%), Space: 8.3 MB (23.74%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
         
+        // counting sort
 //         int r = 0, w = 0, b = 0;
 //         for(auto i : nums){
 //             if(i == 0)r++;
@@ -23,5 +24,22 @@ public:
 //             nums[i]=2;
 //             i++;
 //         }
+        
+        
+        // Dutch National Flag algo | One pass constant space
+        int low = 0, mid = 0, high = nums.size() - 1;
+        
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 2){
+                swap(nums[mid] , nums[high]);
+                high--;
+            }
+            else mid++;
+        }
     }
 };
