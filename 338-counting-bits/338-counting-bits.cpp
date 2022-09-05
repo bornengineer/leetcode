dc1368283b6fbe6220e4d0559507a334d5f22544
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-06 00:49:10 +0530

    Time: 12 ms (32.31%), Space: 26.8 MB (20.97%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,12 +1,35 @@
-// class Solution {
-// public:    
-//     vector<int> countBits(int n) {
-        
-//     }
-// };
-
-
 class Solution {
+public: 
+    // recursive solution
+    int countSetBits(int i){
+        int cnt = 0;
+        while(i > 0){
+            cnt += i % 2;
+            i /= 2;
+        }
+        return cnt;
+    }
+    
+    void helper(int n, vector<int>& ans){
+        if(n == 0){
+            ans.push_back(0);
+            return;
+        }
+        
+        helper(n-1, ans);
+        
+        ans.push_back(countSetBits(n));
+    }
+    
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        helper(n, ans);
+        return ans;
+    }
+};
+
+
+/*class Solution {
 public:
     int countSetBits(int i){
         int cnt = 0;
@@ -29,4 +52,5 @@ public:
         
         return ans;
     }
-};
+ };
+ */
