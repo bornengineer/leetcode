a4b3258b1bc264dd640d5a2cf31366373867d742
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-07 02:15:12 +0530

    Time: 20 ms (9.57%), Space: 13.8 MB (47.19%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     void dfs(int o, int c, vector<string>& res, string s){
-        // cout<<o<<" "<<c<<endl;
+        // cout<<o<<" "<<c<<endl; // debugging
+        
+        // when both gets completely burned we will push the string "s" to res
         if(o == 0 && c == 0){
             res.push_back(s);
             return;
         }
         
-        if(o == c){
+        if(o == c){ // if open & close are same we'll always push a opening one "("
             dfs(o-1, c, res, s + "(");
         }
-        else if(o == 0){
+        else if(o == 0){ // if open = 0 we'll keep pushing the left over closing brackets ")"
             dfs(o, c-1, res, s + ")");      
         }
-        else if(c > o){
+        else if(c > o){ // but when both are non zero & closing > opening then there are two case 
+                        // where we'll push each open & close bracket in different cases
             dfs(o-1, c, res, s + "(");
             
             dfs(o, c-1, res, s + ")");
         }
     }
     
+    // here we will initialise no of both opening & closing brackets with n & burn their values in each recursive call
     vector<string> generateParenthesis(int n) {
         int open = n;
         int close = n;
