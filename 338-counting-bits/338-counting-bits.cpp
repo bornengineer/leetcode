8d7eba9eb339580615db675575ec5f2d23f9f042
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-06 13:48:57 +0530

    Time: 13 ms (26.02%), Space: 7.9 MB (72.63%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public: 
-    // recursive solution
-    int countSetBits(int i){
-        int cnt = 0;
-        while(i > 0){
-            cnt += i % 2;
-            i /= 2;
-        }
-        return cnt;
-    }
-    
-    void helper(int n, vector<int>& ans){
-        if(n == 0){
-            ans.push_back(0);
-            return;
-        }
-        
-        helper(n-1, ans);
-        
-        ans.push_back(countSetBits(n));
-    }
-    
+    // dp solution    
     vector<int> countBits(int n) {
-        vector<int> ans;
-        helper(n, ans);
-        return ans;
+        vector<int> dp(n+1);
+        
+        // starting from 1 cause for 0 it's already 0
+        for(int i = 1; i<= n ; i++){
+            if(i%2 ==0){
+                dp[i] = dp[i/2];
+            }else{
+                dp[i] = dp[i-1] + 1;   
+            }
+        }
+        return dp;
     }
 };
 
+// class Solution {
+// public: 
+//     // recursive solution
+//     int countSetBits(int i){
+//         int cnt = 0;
+//         while(i > 0){
+//             cnt += i % 2;
+//             i /= 2;
+//         }
+//         return cnt;
+//     }
+    
+//     void helper(int n, vector<int>& ans){
+//         if(n == 0){
+//             ans.push_back(0);
+//             return;
+//         }
+        
+//         helper(n-1, ans);
+        
+//         ans.push_back(countSetBits(n));
+//     }
+    
+//     vector<int> countBits(int n) {
+//         vector<int> ans;
+//         vector<int> memo(n+1);
+//         helper(n, ans);
+//         return ans;
+//     }
+// };
 
+
+// iterative solution
 /*class Solution {
 public:
     int countSetBits(int i){
