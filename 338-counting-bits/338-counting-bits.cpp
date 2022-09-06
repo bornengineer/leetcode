3e19b75dbb8f34fb4b917efd585a5361992b5a18
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-06 14:09:19 +0530

    Time: 16 ms (13.80%), Space: 7.9 MB (46.65%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -6,11 +6,19 @@ public:
         
         // starting from 1 cause for 0 it's already 0
         for(int i = 1; i<= n ; i++){
-            if(i%2 ==0){
-                dp[i] = dp[i/2];
-            }else{
-                dp[i] = dp[i-1] + 1;   
-            }
+            // if(i%2 ==0){
+            //     dp[i] = dp[i/2];
+            // }else{
+            //     dp[i] = dp[i-1] + 1;   
+            // }
+            
+            // or 
+            // above approach and below is same if we look them in detail
+            // the i & 1 part will work for odd i's and won't work for even i's
+            // while the dp[i>>1] will work for both
+            // for eg: n = 7 dp[7 >> 1] == dp[3] while in above approach dp[7-1] = dp[6]
+            // it is proven that every even i's set bits are equal to it half's set bits
+            dp[i] = dp[i>>1] + (i & 1);
         }
         return dp;
     }
