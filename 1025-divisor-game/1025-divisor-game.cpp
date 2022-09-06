06e2487108823722c04a2b9930fffccde63add05
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-06 18:54:22 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.32%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        int *dp = new int[n+1];
-        for(int i = 1; i<=n; i++){
-            if(i % 2 == 0)dp[i] = 1;
-            else dp[i] = 0;
-        }
-        return dp[n]==0 ? false : true;
+        // if n is odd return false else return true;
+        return n % 2 == 0;
+        
+        // int *dp = new int[n+1];
+        // for(int i = 1; i<=n; i++){
+        //     if(i % 2 == 0)dp[i] = 1;
+        //     else dp[i] = 0;
+        // }
+        // return dp[n]==0 ? false : true;
     }
 };
