e4c88c021873642ab9d710afc5f29ad99fbc858b
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-08 01:21:17 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (52.06%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
+    // Brute force
+    // here we have used 2N space coz of temp
+    // vector<int> getRow(int rowIndex) {
+    //     vector<int> ans(rowIndex + 1, 0);
+    //     vector<int> temp;
+    //     for(int i = 0; i<ans.size(); i++){
+    //         for(int j = 0 ; j<=i; j++){
+    //             if(j == 0 || j == i){
+    //                 ans[j] = 1;
+    //                 continue;
+    //             }
+    //             ans[j] = temp[j-1] + temp[j];
+    //         }
+    //         temp = ans;
+    //     }
+    //     return ans;
+    // }
+    
+    // optimized
     vector<int> getRow(int rowIndex) {
         vector<int> ans(rowIndex + 1, 0);
-        vector<int> temp;
-        for(int i = 0; i<ans.size(); i++){
-            for(int j = 0 ; j<=i; j++){
-                if(j == 0 || j == i){
-                    ans[j] = 1;
-                    continue;
-                }
-                ans[j] = temp[j-1] + temp[j];
-            }
-            temp = ans;
-        }
+        ans[0] = 1;
+        for(int i = 1; i<ans.size(); i++)
+            for(int j = i ; j >= 1; j--)
+                ans[j] += ans[j-1];
         return ans;
     }
 };
