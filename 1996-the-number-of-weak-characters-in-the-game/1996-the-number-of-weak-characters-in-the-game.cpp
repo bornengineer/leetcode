e4f7fe871d937dfadef0b2814cd513f62301b1e9
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-10 03:17:31 +0530

    Time: 1146 ms (46.51%), Space: 125 MB (91.79%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     static bool comp(vector<int>& a, vector<int>& b){
-        if(a[0] == b[0])return a[1] < b[1];
+        if(a[0] == b[0])return a[1] < b[1]; // if attack is same return lesser one
         
-        return a[0] > b[0];
+        return a[0] > b[0]; // else return bigger attack value coz we're sorting in decreasing order
     }
     
     int numberOfWeakCharacters(vector<vector<int>>& prop) {
         int weak = 0;
         int maxDef = INT_MIN;
-        sort(prop.begin(), prop.end(), comp);
+        // here we will sort it acc to decreasing attack value
+        // then we only need to deal with defense values
+        sort(prop.begin(), prop.end(), comp); // wrote comparator function for decreasing order &
+                                              // to handle equal attack values
         
+        // now in the decreasing order 2d vector if defense values is lesser then we can directly say
+        // that it is a weaker character coz the attack values are already sorted in decreasing order
         for(int i = 0; i<prop.size(); i++){
             if(prop[i][1] < maxDef)weak++;
-            maxDef = max(maxDef,prop[i][1]);
+            else maxDef = max(maxDef,prop[i][1]);
         }
         return weak;
-        
-//         for(auto i : prop){
-//             for(auto j : i){
-//                 cout<<j<<" ";
-//             }
-//             cout<<endl;
-//         }
-        
-        /*
+    }
+};
+
+// brute force by me
+/*
+class Solution {
+public:    
+    int numberOfWeakCharacters(vector<vector<int>>& prop) {
         vector<int> weak(prop.size());
         
         for(int i = 0; i<prop.size(); i++){
@@ -39,6 +43,14 @@ public:
         int cnt = 0;
         for(auto i : weak)cnt += i;
         return cnt;
-        */
     }
 };
+*/
+
+// debug
+        // for(auto i : prop){
+        //     for(auto j : i){
+        //         cout<<j<<" ";
+        //     }
+        //     cout<<endl;
+        // }
