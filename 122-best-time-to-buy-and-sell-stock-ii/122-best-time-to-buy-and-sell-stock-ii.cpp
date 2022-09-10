ebcce0ec790762fb79c3e2351130ecb5fb475040
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-11 02:28:21 +0530

    Time: 24 ms (11.87%), Space: 15.2 MB (24.04%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -17,25 +17,76 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     // RECURSION + MEMOIZATION
+//     int rec(int buy, int idx, vector<int>& prices, int n, vector<vector<int>>& memo){
+//         if(idx == n)return 0;
+//         if(memo[idx][buy] != -1)return memo[idx][buy];
+//         int profit = 0;
+//         if(buy){
+//             // if we buy we are currently in loss (that's why we are using -prices[idx]) and it
+//             // will be replenished by selling the stock and adding a +ve value to it
+//             // below we are taking max of two cases-
+//             // 1. buying the stock
+//             // 2. not buying the stock (same cases are considered in the else block for sell)
+//             profit = max(-prices[idx] + rec(0, idx+1, prices, n, memo) , 0 + rec(1, idx+1, prices, n, memo));   
+//         }
+//         else{
+//             profit = max(prices[idx] + rec(1, idx+1, prices, n, memo) , 0 + rec(0, idx+1, prices, n, memo));   
+//         }
+//         return memo[idx][buy] = profit;
+//     }    
+    
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<int>> memo(n,vector<int>(2,-1));
+//         return rec(1, 0, prices, n, memo);
+//     }
+// };
+
 class Solution {
 public:
-    // RECURSION + MEMOIZATION
+    // DP BOTTOM UP
     int rec(int buy, int idx, vector<int>& prices, int n, vector<vector<int>>& memo){
         if(idx == n)return 0;
         if(memo[idx][buy] != -1)return memo[idx][buy];
         int profit = 0;
         if(buy){
-            profit = max(-prices[idx] + rec(0, idx+1, prices, n, memo) , 0 + rec(1, idx+1, prices, n, memo));    
+            // if we buy we are currently in loss (that's why we are using -prices[idx]) and it
+            // will be replenished by selling the stock and adding a +ve value to it
+            // below we are taking max of two cases-
+            // 1. buying the stock
+            // 2. not buying the stock (same cases are considered in the else block for sell)
+            profit = max(-prices[idx] + rec(0, idx+1, prices, n, memo) , 0 + rec(1, idx+1, prices, n, memo));   
         }
         else{
-            profit = max(prices[idx] + rec(1, idx+1, prices, n, memo) , 0 + rec(0, idx+1, prices, n, memo));    
+            profit = max(prices[idx] + rec(1, idx+1, prices, n, memo) , 0 + rec(0, idx+1, prices, n, memo));   
         }
         return memo[idx][buy] = profit;
     }    
     
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> memo(n,vector<int>(2,-1));
-        return rec(1, 0, prices, n, memo);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        dp[n][0] = dp[n][1] = 0;
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int buy = 0; buy<=1; buy++){
+                int profit = 0;
+                if(buy){
+                    // if we buy we are currently in loss (that's why we are using -prices[idx]) and it
+                    // will be replenished by selling the stock and adding a +ve value to it
+                    // below we are taking max of two cases-
+                    // 1. buying the stock
+                    // 2. not buying the stock (same cases are considered in the else block for sell)
+                    profit = max(-prices[idx] + dp[idx+1][0] , 0 + dp[idx+1][1]);   
+                }
+                else{
+                    profit = max( prices[idx] + dp[idx+1][1] , 0 + dp[idx+1][0]);
+                }
+                dp[idx][buy] = profit;                
+            }
+        }
+        return dp[0][1];
     }
 };
