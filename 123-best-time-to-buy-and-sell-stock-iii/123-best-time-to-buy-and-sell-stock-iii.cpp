e0266d7f37d33c580c1bf79c05a59194a2bb91e3
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-11 21:24:51 +0530

    Time: 618 ms (44.72%), Space: 135.2 MB (44.81%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,40 @@
+// it is same as that of Best Time to Buy and Sell Stock IV but here we are passing 2 to k
+
 // RECURSION + MEMOIZATION
-// class Solution {
-// public:
-//     int rec(int buy, int idx, vector<int>& prices, int n, int k, vector<vector<vector<int>>>& memo){
-//         if(k == 0 || idx == n)return 0;
-//         if(memo[idx][buy][k] != -1)return memo[idx][buy][k];
-//         int profit = 0;
-//         if(buy){
-//             profit = max(-prices[idx] + rec(0, idx+1, prices, n, k, memo), 0 + rec(1, idx+1, prices, n, k, memo));
-//         }
-//         else{
-//             profit = max(prices[idx] + rec(1, idx+1, prices, n, k-1, memo), 0 + rec(0, idx+1, prices, n, k, memo));
-//         }
-//         return memo[idx][buy][k] = profit;
-//     }
-    
-//     int maxProfit(vector<int>& prices, int k = 2) {
-//         int n = prices.size();
-//         vector<vector<vector<int>>> memo (n,vector<vector<int>>(2, vector<int>(k+1, -1)));
-//         return rec(1, 0, prices, n, k, memo);
-//     }
-// };
-
-
-// DP TABULATION || BOTTOM UP
+/*
 class Solution {
 public:
+    // 3D Memoization / DP
+    int rec(int buy, int idx, vector<int>& prices, int n, int k, vector<vector<vector<int>>>& memo){
+        if(k == 0 || idx == n)return 0;
+        if(memo[idx][buy][k] != -1)return memo[idx][buy][k];
+        int profit = 0;
+        if(buy){
+            profit = max(-prices[idx] + rec(0, idx+1, prices, n, k, memo), 0 + rec(1, idx+1, prices, n, k, memo));
+        }
+        else{
+            // when we are done selling the stock our 1 transaction will be done & decrement k by 1;
+            profit = max(prices[idx] + rec(1, idx+1, prices, n, k-1, memo), 0 + rec(0, idx+1, prices, n, k, memo));
+        }
+        return memo[idx][buy][k] = profit;
+    }
+    
+    // it is same as that of Best Time to Buy and Sell Stock IV but here we are passing 2 to k
+    int maxProfit(vector<int>& prices, int k = 2) {
+        int n = prices.size();
+        // 3D vector is created to keep track of all the states 
+        // for eg if idx = n-1 we have to check weather we are allowed to buy or sell ie. buy = 1 or 0?
+        // and in that also we have to keep track of k values (at which value of k we have current ans or is k = 0)
+        vector<vector<vector<int>>> memo (n,vector<vector<int>>(2, vector<int>(k+1, -1)));
+        return rec(1, 0, prices, n, k, memo);
+    }
+};
+
+
+// DP TABULATION || BOTTOM UP (3D DP)
+class Solution {
+public:
+    // it is same as that of Best Time to Buy and Sell Stock IV but here we are passing 2 to k
     int maxProfit(vector<int>& prices, int k = 2) {
         int n = prices.size();
         vector<vector<vector<int>>> dp (n+1,vector<vector<int>>(2, vector<int>(k+1, 0)));
@@ -46,9 +56,33 @@ public:
         return dp[0][1][k];
     }
 };
+*/
+
+// DP TABULATION || BOTTOM UP (2D DP)
+class Solution {
+public:
+    int maxProfit(vector<int>& p, int k = 2) {
+        int n = p.size();
+        vector<vector<int>> dp(n+1, vector<int>(2*k+1, 0));
+        
+        for(int idx = n-1; idx>=0; idx--){
+            for(int tranNo = 2*k -1; tranNo>=0; tranNo--){
+                int profit = 0;
+                if(tranNo % 2 == 0){
+                    profit = max(-p[idx] + dp[idx+1][tranNo+1], dp[idx+1][tranNo]);
+                }
+                else{
+                    profit = max( p[idx] + dp[idx+1][tranNo+1], dp[idx+1][tranNo]);
+                }
+                dp[idx][tranNo] = profit;
+            }
+        }
+        return dp[0][0]; 
+    }
+};
 
 
-
+// my approach - too complicated and didn't work
 // class Solution {
 // public:
 //     int maxProfit(vector<int>& prices) {
