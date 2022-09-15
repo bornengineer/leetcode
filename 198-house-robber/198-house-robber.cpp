096f1c6e6a7ed788c863dea1042962454b5b9fa1
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-16 00:11:52 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.95%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,18 +1,36 @@
-class Solution {
-public:
-    int rec(int idx, vector<int>& nums, vector<int>& dp){
-        if(idx >= nums.size())return 0;
+// Memoization
+// class Solution {
+// public:
+//     int rec(int idx, vector<int>& nums, vector<int>& dp){
+//         if(idx >= nums.size())return 0;
         
-        if(dp[idx] != -1)return dp[idx];
+//         if(dp[idx] != -1)return dp[idx];
         
-                            // pick                     // not pick
-        int ans = max(nums[idx] + rec(idx+2, nums, dp), 0 + rec(idx+1, nums, dp));
+//                             // pick                     // not pick
+//         int ans = max(nums[idx] + rec(idx+2, nums, dp), 0 + rec(idx+1, nums, dp));
         
-        return dp[idx] = ans;
-    }
+//         return dp[idx] = ans;
+//     }
     
+//     int rob(vector<int>& nums) {
+//         vector<int> dp(nums.size(), -1);
+//         return rec(0, nums, dp);
+//     }
+// };
+
+class Solution {
+public:    
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return rec(0, nums, dp);
+        int n = nums.size();
+        if(n==1)return nums[0];
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], nums[0]);
+        
+        for(int i = 2; i<n; i++){
+            dp[i] = max(nums[i] + dp[i-2], 0 + dp[i-1]);
+        }
+        
+        return dp[n-1];
     }
 };
