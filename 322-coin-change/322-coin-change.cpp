2fe7c66767906b385a58475b74866b81b486887e
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-17 00:22:24 +0530

    Time: 305 ms (9.82%), Space: 45.3 MB (26.50%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// Memoization
+/*class Solution {
 public:
     int rec(int idx, vector<int> &coins, int amount, vector<vector<int>> &dp){
         if(idx == 0){
@@ -26,20 +27,55 @@ public:
         if(ans >= 1e9)return -1;
         else return ans;
     }
+};*/
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        // taking amount into consideration cause we have to tacke amount = 0 also
+        vector<vector<int>> dp(n,vector<int>(amount+1, 0));
+        
+        // base case
+        for(int T = 0; T<=amount ; T++){
+            if(T % coins[0] == 0)dp[0][T] = T / coins[0];
+            else dp[0][T] = 1e9;
+        }
+        
+        for(int idx = 1; idx<n; idx++){
+            for(int T = 0; T<=amount; T++){
+                // copy the recurrence & convert to dp
+                int notTake = 0 + dp[idx-1][T];
+        
+                int take = INT_MAX;
+                if(coins[idx] <= T){
+                    take = 1 + dp[idx][T-coins[idx]];
+                }
+
+                dp[idx][T] = min(take, notTake);
+            }
+        }
+        
+        int ans = dp[n-1][amount];
+        if(ans >= 1e9)return -1;
+        else return ans;
+    }
 };
 
+
 // kind of Greedy approach but it failed for - [186,419,83,408] 6249 & [9,6,5,1] 11
-//         int cnt = 0;
-//         int i = coins.size()-1;
-//         sort(coins.begin(), coins.end());
+/*        int cnt = 0;
+        int i = coins.size()-1;
+        sort(coins.begin(), coins.end());
         
-//         while(amount != 0 && i >= 0){
-//             if(amount >= coins[i]){
-//                 amount -= coins[i];
-//                 cnt++;
-//             }
-//             else i--;
-//         }
+        while(amount != 0 && i >= 0){
+            if(amount >= coins[i]){
+                amount -= coins[i];
+                cnt++;
+            }
+            else i--;
+        }
         
-//         if(amount == 0)return cnt;
-//         else return -1;
+        if(amount == 0)return cnt;
+        else return -1;*/
