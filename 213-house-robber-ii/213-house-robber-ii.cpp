9de77aa372ed6b8adeddf8eedb5a397f006d12f6
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-18 13:22:59 +0530

    Time: 0 ms (100.00%), Space: 8 MB (43.41%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -34,14 +34,16 @@ public:
         int n = nums.size();
         if(n == 1)return nums[0];
         
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[1], nums[0]);
+        // vector<int> dp(n, 0);
+        int a = nums[0];
+        int b = max(nums[1], nums[0]);
         
         for(int i = 2 ; i<n; i++){
-            dp[i] = max(0+dp[i-1] , nums[i] + dp[i-2]);
+            int c = b;
+            b = max(0+b , nums[i] + a);
+            a = c;
         }
-        return dp[n-1];
+        return b;
     }
     
     int rob(vector<int>& nums) {
