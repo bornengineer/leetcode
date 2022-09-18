087fbc45d396a6559a1c6d9c111b6a0a4d8424a5
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-18 13:17:19 +0530

    Time: 3 ms (59.36%), Space: 8.1 MB (20.26%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,3 +1,5 @@
+// Memoization
+/*
 class Solution {
 public:
     int rec(int idx, vector<int> &nums, int n, vector<int> &dp){
@@ -23,5 +25,32 @@ public:
         int ans2 = rec(1, nums, n, dp2);
         
         return max(ans1, ans2);
+    }
+};*/
+
+class Solution {
+public:
+    int helper(vector<int> &nums){
+        int n = nums.size();
+        if(n == 1)return nums[0];
+        
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], nums[0]);
+        
+        for(int i = 2 ; i<n; i++){
+            dp[i] = max(0+dp[i-1] , nums[i] + dp[i-2]);
+        }
+        return dp[n-1];
+    }
+    
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)return nums[0];
+        
+        vector<int> temp1 (nums.begin()+1, nums.end());
+        vector<int> temp2 (nums.begin(), nums.end()-1);
+        
+        return max(helper(temp1), helper(temp2));
     }
 };
