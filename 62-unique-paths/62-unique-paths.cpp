38402f7b913177d9c2d0a614fde1e96b52859eba
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-19 23:28:39 +0530

    Time: 9 ms (6.98%), Space: 6.5 MB (46.75%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,4 +1,6 @@
-class Solution {
+// Memoization
+/*
+Solution {
 public:
     int rec(int row, int col, int m, int n, vector<vector<int>> &dp){
         if(row == m-1 && col == n-1)return 1;
@@ -21,5 +23,30 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, -1));
         return rec(0, 0, m, n, dp);
+    }
+};*/
+
+// Tabulation
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int row = 0; row<m; row++){
+            for(int col = 0; col<n; col++){
+                if(row == 0 && col == 0)dp[row][col] = 1;
+                else{
+                    int sameRow = 0;
+                    if(col > 0){
+                        sameRow = dp[row][col-1];
+                    }
+                    int nextRow = 0;
+                    if(row > 0){
+                        nextRow = dp[row-1][col];
+                    }
+                    dp[row][col] = sameRow + nextRow;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
