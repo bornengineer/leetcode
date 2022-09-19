4f7c8c4677f666e13ce7fb81e8972ea72b32468b
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-20 00:33:38 +0530

    Time: 19 ms (37.33%), Space: 10 MB (56.30%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,3 +1,5 @@
+// Memoization
+/*
 class Solution {
 public:
     int rec(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp){
@@ -17,5 +19,29 @@ public:
         int m = grid.size(), n = grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
         return rec(m-1, n-1, grid, dp);
+    }
+};
+*/
+
+// Tabulation
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int row = 0; row<m; row++){
+            for(int col = 0; col<n; col++){
+                if(row == 0 && col == 0) dp[row][col] = grid[0][0];
+                else{   
+                    int top = INT_MAX, left = INT_MAX;
+                    if(row > 0)top = grid[row][col] + dp[row-1][col];
+
+                    if(col > 0)left = grid[row][col] + dp[row][col-1];
+
+                    dp[row][col] = min(top, left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
