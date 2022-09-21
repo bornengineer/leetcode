1c0f1094647e70000a54de107968cd0047da85ca
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-21 13:03:10 +0530

    Time: 739 ms (19.51%), Space: 109.3 MB (14.91%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,4 +1,24 @@
-class Solution {
+class Solution {   
+    public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size(),m = nums2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1, 0));
+        int ans = 0;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                // if the nums match just put 1 + last diagonal element to dp[i][j]
+                if(nums1[i-1] == nums2[j-1])dp[i][j] = 1 + dp[i-1][j-1];
+                // and maintain max ans till now
+                ans = max(ans,dp[i][j]);
+            }
+        }
+        return ans;
+    }
+};
+
+
+// not my code
+/*class Solution {
 public:
     int f(vector<int>& nums1, vector<int>& nums2,int n,int m,vector<vector<int>> &dp){
         if(n==-1 || m==-1) return 0;
@@ -30,4 +50,4 @@ public:
         }
         return res;
     }
-};
+};*/
