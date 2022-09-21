08394fda7903a38a0654f5218a960e2f553c2c8b
M. Amaan Khan <60315143+bornengineer@users.noreply.github.com>
2022-09-21 16:07:57 +0530

    Time: 53 ms (27.31%), Space: 13.2 MB (24.37%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -29,9 +29,9 @@ public:
     int longestCommonSubsequence(string t1, string t2) {
         int n = t1.size();
         int m = t2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        for(int j = 0 ; j<=m; j++)dp[0][j] = 0;  // base case for i
-        for(int i = 0 ; i<=n; i++)dp[i][0] = 0;  // for j
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        // for(int j = 0 ; j<=m; j++)dp[0][j] = 0;  // base case for i
+        // for(int i = 0 ; i<=n; i++)dp[i][0] = 0;  // for j
         
         for(int i = 1; i<=n; i++){
             for(int j = 1; j<=m; j++){
